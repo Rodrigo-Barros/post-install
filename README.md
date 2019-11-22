294c303e921d25ad94b32588d00cf1fb23eda375
Rodrigo Barros <rodrigogenio12@gmail.com>
2019-11-22 07:49:08 -0300

    README adicionado e melhorias no processo de pós instalação

---
+++ README.md
@@ -0,0 +1,1 @@
+esse repositório é dedicado a o script de pós instalação visando tornar uma eventual instalação por qualquer tipo de problema ou mesmo por gostos pessoais um processo mais fácil sem ter que ficar configurando tudo manualmente, esse script é especifico para distribuições baseadas em debian
--- post-install.sh
+++ post-install.sh
@@ -1,33 +1,61 @@
-# create a restore point before start
-echo "atualizando os servidores remotos ..."
-sudo apt update
+#make
+mkdir /tmp/post-install
+mkdir -p $HOME/.local/bin
+#Search packages on local server before look at internet helps to reduce bandwidth usage
+sudo cp apt/detect  /etc/apt/detect-http-proxy
+sudo chmod +x /etc/apt/detect-http-proxy
+sudo cp apt/30detectproxy  /etc/apt/apt.conf.d/30detectproxy
 
-sudo cat apt/detect > /etc/apt/detect-http-proxy
-sudo cat apt/30detectproxy > /etc/apt/apt.conf.d/30detectproxy
+#Monodevelop
+sudo apt install apt-transport-https dirmngr
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 
-sudo apt install timeshift
-if [[ $? -ne 0 ]]; then
-	echo "não foi possível instalar o timeshift, saindo do script..."
-fi
-echo "iniciando o timeshift crie um ponto de restaução antes do script iniciar"
-
-sudo timeshift-gtk
-
-sudo echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" > /etc/apt/sources.list.d/mono-official-stable.list
-sudo echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
-
-# mono repo
 sudo apt install gnupg ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
-# dev-tools section
-sudo apt install apache2 php7.2 php7.2-curl php7.2-cli php7.2-common php7.2-intl php7.2-json php7.2-mbstring php7.2-mysql php7.2-opcache php7.2-readline php7.2-xml code mysql-server mono-devel mono-xsp4
+echo "atualizando os servidores remotos ..."
 
+sudo apt update
+# create a restore point before start
+sudo apt install timeshift
+
+sudo timeshift-gtk
+if [ $? -ne 0 ]; then
+	echo "não foi possível instalar o timeshift, saindo do script..."
+	exit
+fi
+
+# packages section
+develpoment='apache2 php7.2 php7.2-curl php7.2-cli php7.2-common php7.2-intl php7.2-json php7.2-mbstring php7.2-mysql
+	php7.2-opcache php7.2-readline php7.2-xml mysql-server mono-devel mono-xsp4 monodevelop git'
+other='steam zsh todotxt-cli remind'
+
+sudo apt install $develpoment $other
+
+wget -c https://az764295.vo.msecnd.net/stable/8795a9889db74563ddd43eb0a897a2384129a619/code_1.40.1-1573664190_amd64.deb -O $HOME/Downloads/visual-code.deb
+sudo dpkg -i $HOME/Downloads/visual-code.deb
+
+git clone https://github.com/Rodrigo-Barros/dotfiles /tmp/post-install/dotfiles
+
+#oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp /tmp/post-install/dotfiles/.zshrc $HOME/.zshrc
+
+# faster node virtual machine manager fnm
+curl -fsSL https://github.com/Schniz/fnm/raw/master/.ci/install.sh | bash
+#nvim
 wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage -O $HOME/.local/bin/nvim && chmod +x $HOME/.local/bin/nvim
 
-# create custom folders
-mkdir $HOME/.web
-rm -r /var/www/html
-sudo ln -s /var/www/html $HOME/.web
+# recover dotfiles
+cp -r /tmp/post-install/dotfiles/nvim $HOME/.config/
+cp -r /tmp/post-install/dotfiles/prettier $HOME/.config/
+cp -r /tmp/post-install/dotfiles/conky $HOME/.config/conky
+cp -r /tmp/post-install/dotfiles/.todo-txt $HOME/.config/todo-txt
 
+# apache setting
+mkdir $HOME/.web
+sudo rm -r /var/www/html
+sudo ln -s $HOME/.web /var/www/html
